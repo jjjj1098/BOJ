60896ff1ea26d5de848cf2383415ba1effcecf22
Administrator <jjjj1098@naver.com>
2023-08-15 05:14:02 +0900

    fd

---
+++ boj_1271.py
@@ -0,0 +1,5 @@
+n, m = map(int, input().split())
+ans1 = n % m
+ans2 = n // m
+print(ans2)
+print(ans1)
--- boj_1837.py
+++ boj_1837.py
@@ -1,1 +1,11 @@
-P, K = map(int, input().split())
+P, K= map(int, input().split())
+a = True
+for i in range(2,K):
+    if P % i == 0:
+        print('BAD', i)
+        a = False
+        break
+if a:
+    print('GOOD')
+
+
